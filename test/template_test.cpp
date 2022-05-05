e1457561a635c05783a02d5f310e9bb369bc5e5b
clem <clemens.cords@aol.com>
2022-05-06 00:11:51 +0200

    test suited implemented

---
+++ test/template_test.cpp
@@ -0,0 +1,26 @@
+#include <test/test.hpp>
+
+int main()
+{
+    // initialize test environment
+    Test::initialize();
+
+    // actual test behavior, can have more than one testset
+    Test::testset("template_test_set", [](){
+
+        // type 1: assertion basic boolean
+        Test::test(1 + 1 == 2);
+
+        // type 2: assert that function throws
+        Test::test_throws([](){
+            throw std::invalid_argument("always throws");
+        });
+
+        // you can name individual assertions for better debug output
+        bool trigger_fail = false;
+        Test::test(not trigger_fail, "always failed assertion");
+    });
+
+    // always return main like this
+    return Test::conclude();
+}
