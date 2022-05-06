803d57994eef5298101a861038a43476f2ea543d
clem <clemens.cords@aol.com>
2022-05-06 19:39:50 +0200

    sphinx working

--- cmake/Findshaderc_shared.cmake
+++ cmake/Findshaderc_shared.cmake
@@ -1,28 +1,8 @@
-#[=======================================================================[.rst:
 
-Findshaderc_shared
------------
-
-This module is intended to detect the shaderc_shared library.
-If shaderc_shared is not installed globally, the user will have to set
-`SHADER_LIB_DIR` to point to shaderc/lib. If this variable is not yet
-set, a warning will be printed educating users on how to set it.
-
-Imported Targets
-^^^^^^^^^^^^^^^^
-
-``shaderc_shared``
-
-Result Variables
-^^^^^^^^^^^^^^^^
-
-``SHADERC_SHARE_INCLUDE_DIRS``
-    directory pointing to shaderc/include
-
-#]=======================================================================]
-
+# if already available, don't override
 if(NOT DEFINED shaderc_shared)
 
+    # user can specify SHADERC_LIB_DIR to manually specify path
     if (NOT DEFINED SHADERC_LIB_DIR)
         message(WARNING "Unable to detect shaderc_shared library. Make sure it is installed correctly. You can manually specify the path using:\n\t -DSHADERC_LIB_DIR=/path/to/shaderc/lib\n during cmake configuration.")
     endif()
@@ -33,5 +13,6 @@ if(NOT DEFINED shaderc_shared)
     )
 endif()
 
+# deduce include path and export
 get_filename_component(SHADERC_DIR ${shaderc_shared} DIRECTORY)
 set(SHADERC_INCLUDE_DIRS "${SHADERC_DIR}/../include")
