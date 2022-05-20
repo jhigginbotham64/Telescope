62911f0c17bb659f05951ded7f0a10fd1c558e5b
clem <clemens.cords@aol.com>
2022-05-20 18:55:59 +0200

    added input handler

---
+++ include/input_handler.hpp
@@ -0,0 +1,6 @@
+// 
+// Copyright 2022 Clemens Cords
+// Created on 20.05.22 by clem (mail@clemens-cords.com)
+//
+
+#pragma once
---
+++ src/input_handler.cpp
@@ -0,0 +1,7 @@
+// 
+// Copyright 2022 Clemens Cords
+// Created on 20.05.22 by clem (mail@clemens-cords.com)
+//
+
+#include <include/input_handler.hpp>
+
