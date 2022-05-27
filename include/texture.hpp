429587cb15d369815f2bf187a0867a9a3a3099dd
clem <clemens.cords@aol.com>
2022-05-27 18:19:59 +0200

    implemented texture

--- include/texture.hpp
+++ include/texture.hpp
@@ -5,14 +5,96 @@
 
 #pragma once
 
+#include <SDL2/SDL_render.h>
+
+#include <string>
+
+#include <include/color.hpp>
+
 namespace ts
 {
+    //
+    static inline constexpr SDL_PixelFormatEnum PIXEL_FORMAT = SDL_PIXELFORMAT_RGBA32;
+
+    //
+    enum TextureFilteringMode
+    {
+        //
+        NEAREST_NEIGHBOUR = 0,
+
+        //
+        LINEAR = 1,
+
+        //
+        ANISOTROPIC = 2
+    };
+
+    //
+    enum TextureBlendMode
+    {
+        //
+        NONE = SDL_BLENDMODE_NONE,
+
+        //
+        ALPHA = SDL_BLENDMODE_BLEND,
+
+        //
+        ADD = SDL_BLENDMODE_ADD,
+
+        //
+        MULTIPLY = SDL_BLENDMODE_MOD
+    };
+
+    class Window;
+
+    //
     class Texture
     {
         public:
+            //
+            Texture(Window* context);
+
+            //
+            ~Texture();
+
+            //
+            void create(size_t width, size_t height, RGBA color);
+
+            //
+            void load(const std::string& path);
+
+            //
+            void unload();
+
+            //
+            void set_color(RGBA);
+
+            //
+            RGBA get_color() const;
+
+            //
+            void set_blend_mode(TextureBlendMode);
+
+            //
+            TextureBlendMode get_blend_mode() const;
+
+            //
+            void set_filtering_mode(TextureFilteringMode);
+
+            //
+            TextureFilteringMode get_filtering_mode() const;
+
+            //
+            SDL_Texture* get_native();
 
         private:
-            vk::T
+            RGBA _color = RGBA(1, 1, 1, 1);
+            TextureBlendMode _blend_mode = ALPHA;
+            TextureFilteringMode _filtering_mode = LINEAR;
 
+            void update();
+
+            SDL_Renderer* _renderer;
+            SDL_Texture* _texture;
     };
 }
---
+++ src/texture.cpp
@@ -0,0 +1,103 @@
+// 
+// Copyright 2022 Clemens Cords
+// Created on 27.05.22 by clem (mail@clemens-cords.com)
+//
+
+#include <SDL2/SDL_image.h>
+
+#include <include/logging.hpp>
+#include <include/texture.hpp>
+#include <include/window.hpp>
+
+namespace ts
+{
+    Texture::Texture(Window* context)
+        : _renderer(context->get_render_context()), _texture(nullptr)
+    {}
+
+    Texture::~Texture()
+    {
+        unload();
+    }
+
+    void Texture::update()
+    {
+        if (_texture == nullptr)
+            return;
+
+        SDL_SetTextureBlendMode(_texture, (SDL_BlendMode) _blend_mode);
+        SDL_SetTextureColorMod(_texture, _color.red * 255, _color.blue * 255, _color.green * 255);
+        SDL_SetTextureAlphaMod(_texture, _color.alpha * 255);
+    }
+
+    void Texture::create(size_t width, size_t height, RGBA color)
+    {
+        SDL_SetHint("SDL_HINT_RENDER_SCALE_QUALITY", std::to_string((size_t) _filtering_mode).c_str());
+
+        auto* surface = SDL_CreateRGBSurfaceWithFormat(0, width, height, 32, (uint32_t) PIXEL_FORMAT);
+        _texture = SDL_CreateTextureFromSurface(_renderer, surface);
+
+        update();
+
+        SDL_FreeSurface(surface);
+        SDL_ClearHints();
+    }
+
+    void Texture::load(const std::string& path)
+    {
+        SDL_SetHint("SDL_HINT_RENDER_SCALE_QUALITY", std::to_string((size_t) _filtering_mode).c_str());
+        _texture = IMG_LoadTexture(_renderer, path.c_str());
+
+        if (_texture == nullptr)
+            ts::Log::warning("In Texture.load: unable to load texture from file \"", path, "\"");
+
+        update();
+
+        SDL_ClearHints();
+    }
+
+    void Texture::unload()
+    {
+        SDL_DestroyTexture(_texture);
+    }
+
+    void Texture::set_color(RGBA color)
+    {
+        _color = color;
+    }
+
+    RGBA Texture::get_color() const
+    {
+        return _color;
+    }
+
+    void Texture::set_blend_mode(TextureBlendMode mode)
+    {
+        _blend_mode = mode;
+        update();
+    }
+
+    TextureBlendMode Texture::get_blend_mode() const
+    {
+        return _blend_mode;
+    }
+
+    void Texture::set_filtering_mode(TextureFilteringMode mode)
+    {
+        _filtering_mode = mode;
+
+        if (_texture != nullptr)
+            Log::warning("In Texture.set_filtering_mode: Cannot change texture filtering mode if the texture is already GPU-side. Instead, `unload` the texture, set the filtering mode, then `load` the texture again.");
+    }
+
+    TextureFilteringMode Texture::get_filtering_mode() const
+    {
+        return _filtering_mode;
+    }
+
+    SDL_Texture * Texture::get_native()
+    {
+        return _texture;
+    }
+}
+
