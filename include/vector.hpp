d8945e63b536e3315e4c06c5841f2d28d68053c2
Clemapfel <mail@clemens-cords.com>
2022-05-31 15:59:43 +0200

    primed documentation

--- include/vector.hpp
+++ include/vector.hpp
@@ -9,15 +9,30 @@
 
 namespace ts
 {
+    //
     using Vector2f = glm::vec<2, float>;
+
+    //
     using Vector2i = glm::vec<2, int>;
+
+    //
     using Vector2ui = glm::vec<2, size_t>;
 
+    //
     using Vector3f = glm::vec<3, float>;
+
+    //
     using Vector3i = glm::vec<3, int>;
+
+    //
     using Vector3ui = glm::vec<3, size_t>;
 
+    //
     using Vector4f = glm::vec<4, float>;
+
+    //
     using Vector4i = glm::vec<4, int>;
+
+    //
     using Vector4ui = glm::vec<4, size_t>;
 }
